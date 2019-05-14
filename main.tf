fbd00cc985c76c1019dd7e41c4a5f41cc7054229
Jacob <jacobm3@gmail.com>
2019-05-14 15:54:58 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  credentials = "${file("~/.config/gcloud/account.json")}"
+  project     = "hashi-lab"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "image-store" {
+  name     = "jacob-bucket-demo-329048302"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
